809dba4be06f40b654eb2286bb075e18e631ab75
Robert J Kirk <rk@robertjkirk.com>
2023-02-13 22:22:50 -0600

    Login status component for header

---
+++ components/Auth/Login.tsx
@@ -0,0 +1,42 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import { Button, Title, Text, UnstyledButton, Box, Badge, Flex, Spoiler, Loader, Center } from "@mantine/core";
+
+export default function Login() {
+    const { data: session, status } = useSession();
+
+    return (
+        <Box>
+            <Flex justify={"end"}>
+                {status === "loading" && (
+                    <Box style={{ width: 100 }}>
+                        <Center>
+                            <Loader scale="xl" variant="dots" />
+                        </Center>
+                    </Box>
+                )}
+                {status === "unauthenticated" && (
+                    <UnstyledButton onClick={() => signIn()}>
+                        <Badge style={{ width: 100 }} variant="gradient" gradient={{ from: "indigo", to: "cyan", deg: 45 }} size="lg">
+                            SIGN IN
+                        </Badge>
+                    </UnstyledButton>
+                )}
+                {status === "authenticated" && (
+                    <>
+                        <UnstyledButton onClick={() => signOut()}>
+                            <Badge
+                                sx={{
+                                    maxWidth: 200,
+                                }}
+                                variant="gradient"
+                                size="lg">
+                                {session.user.name}
+                            </Badge>
+                        </UnstyledButton>
+                    </>
+                )}
+            </Flex>
+        </Box>
+    );
+}
