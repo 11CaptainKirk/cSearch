ab92be4bbec0b54fb2e1d71e529c9525f31dfc8b
Robert J Kirk <rk@robertjkirk.com>
2023-02-13 22:22:20 -0600

    Typescript configuration reformatting

--- components/ColorSchemeToggle/colorSchemeToggle.tsx
+++ components/ColorSchemeToggle/colorSchemeToggle.tsx
@@ -5,15 +5,15 @@ export function ColorSchemeToggle() {
     const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
     return (
-        <Group position="center" mt="xl">
+        <Group position="center">
             <ActionIcon
                 onClick={() => toggleColorScheme()}
-                size="xl"
+                size="lg"
+                variant="transparent"
                 sx={(theme) => ({
-                    backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
                     color: theme.colorScheme === "dark" ? theme.colors.yellow[4] : theme.colors.blue[6],
                 })}>
-                {colorScheme === "dark" ? <IconSun size={20} stroke={1.5} /> : <IconMoonStars size={20} stroke={1.5} />}
+                {colorScheme === "dark" ? <IconSun size={22} stroke={2} /> : <IconMoonStars size={22} stroke={2} />}
             </ActionIcon>
         </Group>
     );
