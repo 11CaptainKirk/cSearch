74aace8563459d91c938616cb56d875cc07855e6
Robert J Kirk <rk@robertjkirk.com>
2023-02-19 21:40:14 -0600

    Creation of a new part

--- components/Welcome/CreatePart/CreatePart.tsx
+++ components/Welcome/CreatePart/CreatePart.tsx
@@ -3,44 +3,53 @@ import { GetServerSideProps } from "next";
 import { Part } from "@prisma/client";
 import { useForm } from "@mantine/form";
 
-import { Suspense, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { IconHash, IconPlus } from "@tabler/icons";
 import useSWR from "swr";
 import { ComponentType } from "@prisma/client";
 import { getServerSideProps } from "../../../pages/drafts";
 
-function sendPart(part: String) {
-    fetch("/api/part/create", {
+function sendPart(part: String, componentTypeId: Number) {
+    fetch("/api/create/part", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            name: part,
+            partNumber: part,
+            componentTypeId: componentTypeId,
         }),
     });
 }
 
 export default function CreatePart() {
-    //const { data, error, isLoading } = useSWR("/api/category", { refreshInterval: 100000 });
-    const data = [
+    const { data, error, isLoading } = useSWR("/api/category", { refreshInterval: 100000 });
+    let parsedData = [{ value: "JOE", label: "BILL" }];
+    parsedData = data?.map((item) => {
+        return { value: String(item.id), label: item.name };
+    });
+    console.log(parsedData);
+
+    /*const data = [
         { value: "Resistor", label: "Resistor" },
         { value: "Capacitor", label: "Capacitor" },
         { value: "Inductor", label: "Inductor" },
         { value: "Diode", label: "Diode" },
-    ];
+    ];*/
     //console.log(data);
     const form = useForm({
         initialValues: {
             partNum: "",
+            categoryId: "",
         },
 
         validate: {
             partNum: (value) => (value.length < 1 ? "Invalid input" : null),
+            categoryId: (value) => null,
         },
     });
 
-    const [value, setValue] = useState("");
+    //const [value, setValue] = useState("");
     const [open, setOpen] = useState(false);
     return (
         <>
@@ -63,14 +72,34 @@ export default function CreatePart() {
                 }}>
                 <form onSubmit={form.onSubmit((values) => console.log(values))}>
                     <Flex direction="column" gap="md">
-                        {data != undefined && <Select data={data} />}
-                        <TextInput withAsterisk label="Supplier Part #" placeholder="C1010101" {...form.getInputProps("partNum")} icon={<IconHash size={14} />} />
-                        <TextInput withAsterisk label="Supplier Part #" placeholder="C1010101" {...form.getInputProps("name")} icon={<IconHash size={14} />} />
-                        <TextInput withAsterisk label="Supplier Part #" placeholder="C1010101" {...form.getInputProps("value")} icon={<IconHash size={14} />} />
+                        {
+                            <Select
+                                data={parsedData}
+                                searchable
+                                creatable
+                                placeholder="Select a Category"
+                                getCreateLabel={(query) => `+ Create ${query}`}
+                                {...form.getInputProps("categoryId")}
+                                onCreate={async (query) => {
+                                    let res = await fetch("/api/category", {
+                                        method: "POST",
+                                        headers: {
+                                            "Content-Type": "application/json",
+                                        },
+                                        body: JSON.stringify({
+                                            name: query,
+                                        }),
+                                    });
+                                    console.log(res);
+                                    parsedData.push({ value: "2", label: query });
+                                }}
+                            />
+                        }
+                        <TextInput withAsterisk label="JLC Part #" placeholder="C1010101" {...form.getInputProps("partNum")} icon={<IconHash size={14} />} />
                         <Space h="md" />
                     </Flex>
                     <Center>
-                        <Button type="submit" variant="outline" onClick={() => sendPart(value)}>
+                        <Button type="submit" variant="outline" onClick={() => sendPart(form.values.partNum, Number(form.values.categoryId))}>
                             Create Part
                         </Button>
                     </Center>
